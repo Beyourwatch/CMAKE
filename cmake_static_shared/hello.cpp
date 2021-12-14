57d014b5501779754c8d0367d327e9aafa54d5c2
Beyourwatch <Beyourwatch@163.com>
2021-12-15 00:07:28 +0800

    add code statis and shared lib

---
+++ cmake_static_shared/hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+void hello(){
+
+ std::cout << "hello world" << std::endl;
+
+}
---
+++ cmake_static_shared/main.cpp
@@ -0,0 +1,10 @@
+#include "hello.h"
+
+int main() {
+
+ char c;
+ hello();
+ std::cin>> c ;
+ return 0;
+
+}
